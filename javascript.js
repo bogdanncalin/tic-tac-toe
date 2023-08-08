df6f01bdb2cfd446d340c1e344944e0627339d74
bogdanncalin <bogdanncalin@yahoo.com>
2023-08-08 21:08:18 +0700

    Finished the game

--- javascript.js
+++ javascript.js
@@ -1,73 +1,189 @@
-
 let gameBoard = {
-    cellsArray : [],
+    _cellsArray: [],
 
-    create : function () {
-        console.log('test');
-        let cellsArray = [];
-        for(var y = 0; y < 3; y++) {
-            for(var x = 0; x < 3; x++)    {
-            this.cellsArray.push(new Cell(x, y, ''));
+    create: function () {
+        this._cellsArray = [];
+        for (var y = 0; y < 3; y++) {
+            for (var x = 0; x < 3; x++) {
+                this._cellsArray.push(new Cell(x, y, ''));
             }
         }
+        return this._cellsArray;
+    },
 
-        return this.cellsArray;
+    get cellsArray() {
+        return this._cellsArray;
     }
 }
 
 class Cell {
-    constructor(positionX, positionY, content, isEmpty){
+    constructor(positionX, positionY, content, isEmpty) {
         this.positionY = positionY;
         this.positionX = positionX;
-        this.content = content; // shows if the cell is empt
-        if(typeof this.isEmpty === 'boolean'){
-            this.isEmpty = false;
-        }
-        this.isEmpty = typeof this.isEmpty === 'boolean' ? isEmpty : false;
+        this.content = content; // shows if the cell is empty, has X or a 0 in it
     }
-
-// to be implemented
-/*
-    clicked(){
-        if(x clicked)
-            content change to x
-        else
-            content change to y
-    }
-*/
 }
 
-class Players{
-    constructor(name, side){
+class Players {
+    constructor(name, side) {
         this.name = name;
         this.side = side; // the player is either X or O
     }
 }
 
-let game = {
-
-    // to be implemented
-    /*
-    play : function () {
-        starts the game
-    }
-    */
-}
+let gameBoardElement = document.querySelector('.game-board');
 
 function renderGameBoard() {
+    const cellsArray = gameBoard.create();
 
-// !!! TO DO: check the implementation, doesn't work properly
-
-    // to be implemented
-    /*
-    renders the gameboard in the page
-    */
-   let gameBoard = document.querySelector('.game-board');
-
-   for(let i = 0; i < 9; i++){
+    for (let i = 0; i < cellsArray.length; i++) {
         let square = document.createElement('div');
-        gameBoard.appendChild(square);
-   }
+        square.classList.add('square');
+        square.setAttribute('id', i);
+        square.textContent = cellsArray[i].content;
+        square.setAttribute('data-cell-index', i);
+        gameBoardElement.appendChild(square);
+    }
 }
 
+function play() {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('click', squareClicked);
+    });
+}
+
+var playerX = new Players('Name1', 'x');
+var playerO = new Players('Name2', 'o');
+
+var currentPlayer = playerX;
+var turn = playerX;
+
+function showWinnerModal(winner) {
+    const modal = document.getElementById('winModal');
+    const winnerMessage = document.getElementById('winnerMessage');
+    const closeModalButton = document.getElementById('closeModal');
+
+    winnerMessage.textContent = winner;
+    modal.style.display = 'flex';
+
+    closeModalButton.addEventListener('click', function() {
+        modal.style.display = 'none';
+        refreshBoard();
+    });
+}
+
+function tieGame(){
+    showWinnerModal('It is a tie..');
+}
+
+function youWon(){
+    if(currentPlayer === playerO){
+        showWinnerModal('Player X has won');
+
+    }
+    else {
+        showWinnerModal('Player O has won');
+    }
+}
+
+function checkWinRows() {
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+        const rowStartIndex = i * 3;
+        if (
+            gameBoard.cellsArray[rowStartIndex].content !== '' &&
+            gameBoard.cellsArray[rowStartIndex].content === gameBoard.cellsArray[rowStartIndex + 1].content &&
+            gameBoard.cellsArray[rowStartIndex + 1].content === gameBoard.cellsArray[rowStartIndex + 2].content
+        ) {
+            return true; // Row win condition is met
+        }
+    }
+    return false; // No win condition is met
+}
+
+function checkWinColumns() {
+    for (let i = 0; i < 3; i++) {
+        if (
+            gameBoard.cellsArray[i].content !== '' &&
+            gameBoard.cellsArray[i].content === gameBoard.cellsArray[i + 3].content &&
+            gameBoard.cellsArray[i + 3].content === gameBoard.cellsArray[i + 6].content
+        ) {
+            return true; // Column win condition is met
+        }
+    }
+    return false; // No win condition is met
+}
+
+function checkWinDiagonals() {
+    if (
+        (gameBoard.cellsArray[0].content !== '' &&
+        gameBoard.cellsArray[0].content === gameBoard.cellsArray[4].content &&
+        gameBoard.cellsArray[4].content === gameBoard.cellsArray[8].content) ||
+        (gameBoard.cellsArray[2].content !== '' &&
+        gameBoard.cellsArray[2].content === gameBoard.cellsArray[4].content &&
+        gameBoard.cellsArray[4].content === gameBoard.cellsArray[6].content)
+    ) {
+        return true; // Diagonal win condition is met
+    }
+    return false; // No win condition is met
+}
+
+function checkTie() {
+    for (const cell of gameBoard.cellsArray) {
+        if (cell.content === '') {
+            return false; // At least one cell is empty, not a tie
+        }
+    }
+    return true; // All cells are filled, it's a tie
+}
+
+
+function squareClicked(event) {
+    const square = event.target;
+    const cellIndex = parseInt(square.getAttribute('data-cell-index'), 10);
+    const cell = gameBoard.cellsArray[cellIndex];
+
+    if(cell.content === ''){
+        if (currentPlayer === playerX) {
+            cell.content = 'X';
+            currentPlayer = playerO;
+        } else if (currentPlayer === playerO) {
+            cell.content = 'O';
+            currentPlayer = playerX;
+        }
+    }
+
+    square.textContent = cell.content;
+    
+    if(checkWinColumns() || checkWinRows() || checkWinDiagonals()){
+        youWon();
+    }
+    else if (checkTie()){
+        tieGame();
+    }
+}
+
+function refreshBoard() {
+    currentPlayer = playerX;
+    let squares = document.querySelectorAll('.square');
+    for(let i = 0; i <= 8; i++){
+            gameBoard.cellsArray[i].content = '';
+        }
+        
+    squares.forEach((square) => {
+            square.textContent = '';
+        });
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    const refreshButton = document.querySelector('#refreshButton');
+
+    let squares = document.querySelectorAll('.square');
+    refreshButton.addEventListener('click', function() {
+        refreshBoard();
+    });
+});
+
 renderGameBoard();
+play();
